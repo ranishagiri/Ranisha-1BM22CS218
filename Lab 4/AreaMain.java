5b7c555844526bccc112719dddc14182666c6618
ranishagiri <153653459+ranishagiri@users.noreply.github.com>
2024-02-23 22:42:35 +0530

    Update AreaMain.java

--- Lab 4/AreaMain.java
+++ Lab 4/AreaMain.java
@@ -41,7 +41,7 @@ class Circle extends Shape{
 
 class AreaMain{
 	public static void main(String args[]){
-		System.out.println("Pranav Y - 1BM22CS204");
+		System.out.println("Ranisha Giri - 1BM22CS218");
 		Rectangle r = new Rectangle();
 		Triangle tr = new Triangle();
 		Circle c = new Circle();
